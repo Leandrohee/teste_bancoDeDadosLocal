2898298438710e96f2180ffb6c2ecce741e54d25
leandroTorres <leandrohenrique_@live.com>
2023-07-10 12:14:46 -0300

    terminei meu teste

--- app/app.js
+++ app/app.js
@@ -1,6 +1,59 @@
-import { conectaApi, respostaJson } from "./conectApi.js"
+import { conectaApi, addDadosApi, atualizaDadosApi } from "./conectApi.js"
 
-const titulo = document.querySelector(".titulo")
+const formulario = document.querySelector(".formulario")
+var tabela = document.querySelector(".tabela")
 
-conectaApi()
-console.log(respostaJson)
+//conexao com a api
+const conexaoApi = await conectaApi()
+const eApi = conexaoApi.length
+const proximoId = eApi +1
+
+// Add novo dado pelo formulario
+formulario.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const inputnome = document.querySelector('.nome').value
+    const inputidade = document.querySelector('.idade').value
+    const inputprofissao = document.querySelector('.profissao').value
+
+    addDadosApi(proximoId,inputnome,inputidade,inputprofissao)
+})
+
+
+//mostra a tabela atualizada
+conexaoApi.forEach(element => {
+    tabela.innerHTML = tabela.innerHTML + `
+        <tr data-id=${element.id}>
+            <td>${element.nome}</td>
+            <td>${element.idade} anos</td>
+            <td>${element.profissao}</td>
+            <td><input data-inputnomeedit${element.id} placeholder="nome" type="text"></td>
+            <td><input data-inputidadeedit${element.id} placeholder="idade" type="text"></td>
+            <td><input data-inputprofissaoedit${element.id} placeholder="profissao" type="text"></td>
+            <td><button class="btn-edit">editar</button></td>
+        </tr>
+    `
+});
+
+
+//Edita os dados
+const btnseditar = document.querySelectorAll(".btn-edit")
+btnseditar.forEach(btn =>{
+    btn.addEventListener("click",(e)=>{
+        const idreferente = e.target.parentNode.parentNode.dataset.id
+        const inputnomereferente =  document.querySelector(`[data-inputnomeedit${idreferente}]`).value
+        const inputidadereferente = document.querySelector(`[data-inputidadeedit${idreferente}]`).value
+        const inputprofissaoreferente = document.querySelector(`[data-inputprofissaoedit${idreferente}]`).value
+
+        console.log(idreferente)
+        console.log(inputnomereferente)
+        console.log(inputidadereferente)
+        console.log(inputprofissaoreferente)
+
+        atualizaDadosApi(
+            idreferente,
+            inputnomereferente,
+            inputidadereferente,
+            inputprofissaoreferente
+            )
+    })
+})
--- app/conectApi.js
+++ app/conectApi.js
@@ -1,8 +1,36 @@
-export var respostaJson
-
-export async function conectaApi(){
+export async function conectaApi(){                                 //le os dados da api
     const resposta = await fetch("http://localhost:3000/filhos")
-    respostaJson = await resposta.json()
+    const respostaJson = await resposta.json()
+
+    return respostaJson
+}
+
+export async function addDadosApi(id,nome,idade,profissao){             //add dados na api
+    const resposta = await fetch("http://localhost:3000/filhos",{
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            id: id,
+            nome: nome,
+            idade: idade,
+            profissao: profissao,
+        })
+    })
+}
+
+
+export async function atualizaDadosApi(id,nome,idade,profissao){            //atualiza dados na api
+    const resposta = await fetch(`http://localhost:3000/filhos/${id}`,{
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            id: id,
+            nome: nome,
+            idade: idade,
+            profissao: profissao,
+        })
+    })
 
 }
+
 
